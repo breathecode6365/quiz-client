b44a291d39d7d2cc4265f3ad08cbad1626a7c180
breathecode6365 <pdandlur@gitam.in>
2024-05-06 20:05:29 +0530

    initial commit

---
+++ src/pages/Game.js
@@ -0,0 +1,170 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Game = ({ socket, username, myRoom, setMyRoom }) => {
+  const [startCnt, setStartCnt] = useState(10);
+  const [cntShow, setCntShow] = useState(false);
+  const [showQuiz, setShowQuiz] = useState(false);
+  const [curQuestion, setCurQuestion] = useState(null);
+  const [answer, setAnswer] = useState("");
+  const [questionTime, setQuestionTime] = useState(10);
+  const [showResult, setShowResult] = useState(false);
+  let answerMap = {
+    roomId: myRoom.roomId,
+    user: username,
+    questions: {},
+  };
+  const answerKey = useRef(new Map());
+
+  useEffect(() => {
+    socket.on("gameOver", () => {
+      const myObject = Object.fromEntries(answerKey.current);
+      answerMap.questions = myObject;
+      setShowQuiz(false);
+      console.log("Game Over ------->", answerMap);
+      socket.emit("gameOver", answerMap);
+    });
+    socket.on("startGame", (roomData) => {
+      setMyRoom(roomData);
+      setCntShow(true);
+      socket.emit("ready", myRoom);
+      let count = 10;
+      const countdownInterval = setInterval(() => {
+        count--;
+        setStartCnt(count);
+        if (count === 0) {
+          clearInterval(countdownInterval);
+          setShowQuiz(true);
+          setCntShow(false);
+        }
+      }, 1000);
+
+      return () => clearInterval(countdownInterval);
+    });
+
+    socket.on("question", (newQuestion) => {
+      console.log("answerKey ---> ", answerKey);
+      setCurQuestion(newQuestion);
+      setAnswer("");
+      setQuestionTime(10);
+    });
+    socket.on("endGame", (roomData) => {
+      setMyRoom(roomData);
+      setShowResult(true);
+      console.log("Result Room ", roomData);
+    });
+    socket.on("leftRoom",(mssg)=>{
+      alert(mssg)
+      window.location.href="/";
+    })
+  }, [socket]);
+
+  useEffect(() => {
+    const questionTimeInterval = setInterval(() => {
+      setQuestionTime((prevTime) => prevTime - 1);
+    }, 1000);
+    return () => clearInterval(questionTimeInterval);
+  }, [curQuestion]);
+
+  return (
+    <div className="w-screen h-screen flex flex-col gap-3 items-center justify-center ">
+      {!showQuiz && (
+        <div className="md:w-[600px] w-[320px] overflow-hidden   bg-white p-5 rounded-lg shadow-xl">
+          <div className="border-2 border-green-800">
+            <div className="w-full h-[50px] bg-green-800 flex items-center justify-center">
+              <h1 className="text-white text-center text-2xl font-bold">
+                Quizzy
+              </h1>
+            </div>
+            <div className="flex flex-col gap-4 p-2">
+              <h1 className="text-sm mt-2">
+                Room Code:{" "}
+                <span className="text-[12px] p-1 border-2 border-black bg-gray-400 ">
+                  {myRoom.roomId}
+                </span>
+              </h1>
+              <div className="flex flex-row justify-between">
+                <p className="text-sm font-semibold border border-blue-900 bg-blue-400 p-2 rounded-lg">{`${username} (you)`}</p>
+                <p className="text-sm font-semibold border border-red-900 bg-red-400 p-2 rounded-lg">
+                  {(myRoom.players[0].playerName === username)? myRoom.players[1]?(myRoom.players[1].playerName):"Waiting for player" : myRoom.players[0].playerName}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {cntShow && (
+        <h1 className="border-2 border-black p-2 font-bold">
+          Game will start in:{" "}
+          <span className="text-xl text-green-600 font-bold">{startCnt}</span>
+        </h1>
+      )}
+      {showQuiz && curQuestion && (
+        <div className="p-3">
+          <div className="flex flex-col items-center justify-center gap-2 p-4 bg-white w-[320px] rounded-xl border-2 border-blue-700">
+            <div className="rounded-[50%] border-2 border-black flex items-center justify-center w-[30px] h-auto">{questionTime}</div>
+            <h2 className="font-bold text-wrap">{curQuestion?.question}</h2>
+            <ul className="flex flex-col gap-3
+            ">
+              {curQuestion.options?.map((option, index) => (
+                <li key={index} className="bg-blue-300 
+                hover:bg-blue-600 focus:bg-green-500 flex items-center p-2 rounded-xl ">
+                  <input
+                  className="mr-3 my-3"
+                    type="radio"
+                    id={`option${index}`}
+                    name="answer"
+                    value={option}
+                    checked={answer === option}
+                    onChange={(e) => {
+                      answerKey.current.set(
+                        curQuestion.questionId,
+                        e.target.value
+                      );
+                      setAnswer(e.target.value);
+                    }}
+                  />
+                  <label htmlFor={`option${index}`}>{option}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+      {showResult && (
+        <div className="w-[310px] flex flex-col items-center justify-center bg-white shadow-xl rounded-xl p-3">
+          <div className="border-2 border-green-800 w-full flex flex-col text-center justify-center items-center py-5">
+            <h1 className="text-green-800 font-bold text-2xl">Game Over</h1>
+            <h2 className="text-lg font-bold">Results</h2>
+            {myRoom.stats.draw ? (
+              <h2 className="text-xl font-bold bg-blue-300 border-2 border-blue-600 p-3">
+                Draw
+              </h2>
+            ) : (
+              <h2>
+                <span
+                  className={`${
+                    username === myRoom.winner
+                      ? "bg-green-700 text-lg font-bold"
+                      : "bg-red-700 text-lg font-bold"
+                  }`}>
+                  {myRoom.stats.winner}
+                </span>{" "}
+                is the winner!
+              </h2>
+            )}
+            {myRoom.players.map((player) => (
+              <p key={player.playerName}>
+                {player.playerName} : {player.score}
+              </p>
+            ))}
+            <a href="/" className="border-b-2 border-blue-900 text-blue-900">
+              play again
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/pages/Home.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import Game from "./Game";
+
+const socket = io("https://quiz-server-g48l.onrender.com");
+
+const Home = () => {
+  const [show, setShow] = useState(false);
+  const [username, setUsername] = useState("");
+  const [myRoom, setMyRoom] = useState(null);
+  const [roomId, setRoomId] = useState("");
+  useEffect(() => {
+    {
+      socket.on("leftRoom",(mssg)=>{
+        alert(mssg)
+        window.location.href="/";
+      })
+      socket.on("joined", (roomData) => {
+        setMyRoom(roomData);
+        setShow(true);
+        console.log(roomData);
+      });
+      socket.on("error",(mssg)=>{
+        alert(mssg);
+      })
+      socket.on("startGame", () => {
+        setTimeout(() => {}, 5000);
+      });
+    }
+  }, [socket]);
+  const joinServer = (e) => {
+    e.preventDefault();
+    if (username !== "") {
+      socket.emit("join", username);
+    }
+  };
+  const joinRoom = (e) => {
+   e.preventDefault();
+   if (username !== "") {
+     socket.emit("joinPrivate", username);
+   }
+  };
+  const joinByRoom = (e) => {
+    e.preventDefault();
+    if (roomId !== "") {
+      socket.emit("joinByRoom", roomId, username);
+    }
+  };
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      {!show ? (
+        <div className="bg-white w-[300px] h-max p-5 rounded-xl flex flex-col items-center text-center shadow-xl gap-3">
+          <div className="m-5">
+            <h1 className="text-green-800  font-extrabold text-3xl ">Quizzy</h1>
+          </div>
+          <form onSubmit={joinServer}>
+            <input
+              type="text"
+              className="border-2 border-gray-300 p-2 rounded-lg w-full mb-5 focus:outline-none focus:border-green-500"
+              value={username}
+              placeholder="Enter your name"
+              autoComplete="off"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <button className="bg-green-800 text-white w-full py-2 rounded-lg">
+              Join Server</button>
+          </form>
+           
+          <a className="mt-2 text-xs" href="https://www.punithdandluri.me">
+            Developed By <span className="text-red-500">Punith Dandluri</span>
+          </a>
+        </div>
+      ) : (
+        <Game
+          myRoom={myRoom}
+          socket={socket}
+          username={username}
+          setMyRoom={setMyRoom}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Home;
